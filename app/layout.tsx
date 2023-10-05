d40776793bd57d2bda80637aa66794387c97b64d
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-10-06 02:30:15 +0530

    refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
           integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
         />
       </head>
       <body className={inter.className}>{children}</body>
