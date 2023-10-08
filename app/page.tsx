96d8bf739c6f623ee8989b67928afc4e4faa89f2
Shubham Singh <shubham212a@gmail.com>
2023-10-08 23:59:50 +0530

    fixed Folder Structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,7 @@
-import Button from "@/components/Button";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import NavBar from "@/components/NavBar";
+import Section_1 from "./(Home)/Section1";
 
-export default function Home() {
+export default function page() {
   return (
-    <div className="min-h-screen">
-      <div className="p-4">
-        <Header />
-        <div className="p-24">
-          <div className="flex flex-col gap-4 text-7xl font-medium">
-            <p>ANDROID CLUB</p>
-            <p>VIT CHENNAI</p>
-            <hr className="text-white border-2 border-white w-32" />
-          </div>
-          <div className="mt-6 flex items-center gap-4">
-            <Button text="UPCOMING EVENTS" />
-            <p>LEARN MORE</p>
-          </div>
-          <div className="flex flex-col gap-4 text-xl mt-6">
-            <i className="fa-brands fa-twitter" />
-            <i className="fa-brands fa-instagram" />
-            <i className="fa-brands fa-linkedin" />
-          </div>
-        </div>
-        <NavBar />
-      </div>
-      <Footer />
-    </div>
+    Section_1()
   );
 }
