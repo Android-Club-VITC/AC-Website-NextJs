96507d8282ab7ab99a3330f21761a01ef25a6253
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-10-13 17:31:26 +0530

    add events page

---
+++ constants/events.tsx
@@ -0,0 +1,32 @@
+import webverse from '@assets/events/webverse.png'
+import recruitment from '@assets/events/recruitment.png'
+import krypthon from '@assets/events/krypthon.png'
+import techknowcamp from '@assets/events/tech-know-camp.png'
+
+export const events = [
+  {
+    name: 'Web Verse',
+    description:
+      'WEB DEVELOPMENT HACKATHON & SEMINAR WITH OVER 400+ PARTICIPATION FROM ALL OVER INDIA',
+    img: webverse,
+  },
+  {
+    name: 'Fresher Recruitment',
+    description:
+      'RECRUITMENT OF JUNIORS TO FORM THE CLUB TEAM FOR TERM 2023-2024',
+    img: recruitment,
+  },
+  {
+    name: 'Krypthon Hackathon',
+    description: 'HACKATHON WITH MULTIPLE DOMAINS',
+    img: krypthon,
+  },
+]
+
+export const upcomingEvent = {
+    name: 'Tech Know Camp',
+    description:
+        'Meticulously designed to address the questions and doubts you may have about your academic and career path. Whether you are exploring programming languages 💻 or seeking a clear direction for your future, our event is tailor-made to equip you with the knowledge you need. To make your participation even more convenient, we kindly invite you to register for this valuable event. The registration link is provided along with the event poster in this email. 📝',
+    img: techknowcamp,
+    url: 'https://vtopcc6.vit.ac.in/chennai-events/eventPreview/368',
+}
