012a00d133f6fa6127a8ee05107fd6b68ef128d3
shubhamm69 <shubham212a@gmail.com>
2023-10-16 21:39:59 +0530

    removed alice Carousel

--- app/(Home)/Section3.tsx
+++ app/(Home)/Section3.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from 'react'
-import 'react-alice-carousel/lib/alice-carousel.css'
 import { SectionWrapper } from '@/components/HOC'
 import ResponsiveCarousel from '@/components/OurTeam/TeamSlider'
 
