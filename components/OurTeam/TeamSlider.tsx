07f413a7cbbca6cf18cadd206b76b301c34397e4
Pranshu Aggarwal <70687348+Pranshu1902@users.noreply.github.com>
2023-10-17 01:32:03 +0530

    Update TeamSlider.tsx

--- components/OurTeam/TeamSlider.tsx
+++ components/OurTeam/TeamSlider.tsx
@@ -1,46 +1,46 @@
 import { values } from '@/constants/ourTeam'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-import { Carousel } from 'react-responsive-carousel'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import Image from 'next/image'
 
-export default function ResponsiveCarousel() {
+const ResponsiveCarousel = () => {
+  const settings = {
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  }
+
   return (
-    <div className="flex flex-col justify-center items-center rounded-lg overflow-hidden p-4 shadow-md">
-      <Carousel
-        centerMode
-        centerSlidePercentage={50}
-        showArrows={true}
-        autoPlay={true}
-        showIndicators={true}
-        infiniteLoop={true}
-        dynamicHeight={false}
-        showThumbs={false}
-        className="w-full"
-      >
+    <div>
+      <Slider {...settings} className="slick-container">
         {values.map((value) => (
-          <div
-            key={value.name}
-            className="flex flex-col md:flex-row items-center justify-center gap-10 p-5 rounded-xl bg-secondary m-10 h-full md:h-72"
-          >
-            <div className="w-1/3">
-              <Image
-                src={value.image}
-                alt="avatar"
-                className="rounded-full object-cover bg-white h-44 w-44"
-              ></Image>
-            </div>
-            <div className="px-4 py-2 border-l-2 border-gray-500 w-full  md:w-2/3">
-              <p className="name text-primary text-3xl">{value.name}</p>
-              <p className="designation text-textShade2 text-xl">
-                {value.role}
-              </p>
-              <p className="description mt-4 text-gray-300">
-                {value.description}
-              </p>
+          <div key={value.name} className="slick-slide">
+            <div className="flex flex-col md:flex-row items-center justify-center p-5 rounded-xl bg-secondary m-10 h-full md:h-72">
+              <div className="w-1/3">
+                <Image
+                  src={value.image}
+                  alt="avatar"
+                  className="rounded-full object-cover bg-white h-44 w-44"
+                />
+              </div>
+              <div className="px-4 py-2 border-l-2 border-gray-500 w-full md:w-2/3">
+                <p className="name text-primary text-3xl">{value.name}</p>
+                <p className="designation text-textShade2 text-xl">
+                  {value.role}
+                </p>
+                <p className="description mt-4 text-gray-300">
+                  {value.description}
+                </p>
+              </div>
             </div>
           </div>
         ))}
-      </Carousel>
+      </Slider>
     </div>
-  );
+  )
 }
+
+export default ResponsiveCarousel
