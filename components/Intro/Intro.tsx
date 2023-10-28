70b8aed754794a6e33f879620bda6f347613a5bd
Shikhar_Burman <134587780+StephenStolk@users.noreply.github.com>
2023-10-28 15:30:10 +0530

    Create Intro containing the text section

---
+++ components/Intro/Intro.tsx
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <>
+      <div
+        className="hidden w-full lg:w-1/2 sm:flex sm:flex-col justify-center p-12 sm:text-6xl text-center md:text-center xl:text-start lg:text-9xl flex-wrap     
+      gap-4 text-5xl md:text-8xl font-medium"
+      >
+        VIT CHENNAI ANDROID CLUB
+        <p className="text-xl sm:text-2xl xl:text-4xl font-medium mt-4">
+          WE ARE HERE AT YOUR SERVICE!
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Intro
